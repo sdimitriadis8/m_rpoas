dcf94e38814f6768af105e9717739abf33c2e250
Sokratis Dimitriadis <info@28x.gr>
2025-05-22 18:56:09 +0300

    Format Python code with black (PEP8 cleanup)

--- launch/spawn_bot.launch.py
+++ launch/spawn_bot.launch.py
@@ -5,29 +5,25 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 import os
 
+
 def generate_launch_description():
     urdf_file = os.path.join(
-        get_package_share_directory('mrpoas_core'),
-        'urdf',
-        '4wd_patrol_bot.urdf'
+        get_package_share_directory("mrpoas_core"), "urdf", "4wd_patrol_bot.urdf"
     )
 
-    return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            parameters=[{'robot_description': open(urdf_file).read()}]
-        ),
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher'
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen'
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                parameters=[{"robot_description": open(urdf_file).read()}],
+            ),
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+            ),
+            Node(package="rviz2", executable="rviz2", name="rviz2", output="screen"),
+        ]
+    )
--- setup.py
+++ setup.py
@@ -2,28 +2,29 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'mrpoas_core'
+package_name = "mrpoas_core"
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=find_packages(exclude=['test']),
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-    ('share/ament_index/resource_index/packages',
-        ['resource/mrpoas_core']),
-    ('share/mrpoas_core', ['package.xml']),
-    ('share/mrpoas_core/launch', ['launch/spawn_bot.launch.py']),
-    ('share/mrpoas_core/urdf', ['urdf/2wd_patrol_bot.urdf','urdf/4wd_patrol_bot.urdf']),
-],
-    install_requires=['setuptools'],
+        ("share/ament_index/resource_index/packages", ["resource/mrpoas_core"]),
+        ("share/mrpoas_core", ["package.xml"]),
+        ("share/mrpoas_core/launch", ["launch/spawn_bot.launch.py"]),
+        (
+            "share/mrpoas_core/urdf",
+            ["urdf/2wd_patrol_bot.urdf", "urdf/4wd_patrol_bot.urdf"],
+        ),
+    ],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='sokratis',
-    maintainer_email='info@28x.gr',
-    description='Core system of the m-rpoas patrol robot',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="sokratis",
+    maintainer_email="info@28x.gr",
+    description="Core system of the m-rpoas patrol robot",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
